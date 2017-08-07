7bd3303a212e75cd6368adc8891b28b60c2ee34e
tereg <teregalla@gmail.com>
2017-08-07 11:22:29 -0700

    Add silhouette image and styling to Welcome component.

--- components/Welcome.js
+++ components/Welcome.js
@@ -11,7 +11,7 @@ export default class Welcome extends React.Component {
     return (
       <View style={styles.welcomeContainer}>
         <Image
-          source={ require('../assets/images/robot-dev.png') }
+          source={ require('../assets/images/silhouette.jpg') }
           style={styles.welcomeImage}
         />
         <Text style={styles.welcomeText}>Welcome to your place of rest.</Text>
@@ -26,5 +26,12 @@ const styles = StyleSheet.create ({
   },
   welcomeText: {
     fontFamily: "Futura",
+    textAlign: "center",
+    fontSize: 20,
+    paddingTop: 10
+  },
+  welcomeImage: {
+    height: 300,
+    width: 600
   }
 })
