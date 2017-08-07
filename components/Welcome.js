e72222ae475fb10c5a631d93a6fadacabf6f5d8b
tereg <teregalla@gmail.com>
2017-08-07 12:55:16 -0700

    Create Button component.
    Add buttons to Welcome component.
    Add styling to Button component and two buttons on Welcome component.

--- components/Welcome.js
+++ components/Welcome.js
@@ -5,24 +5,41 @@ import {
   Text,
   View,
 } from 'react-native';
+import Button from './common/Button';
 
 export default class Welcome extends React.Component {
   render() {
     return (
       <View style={styles.welcomeContainer}>
-        <Image
-          source={ require('../assets/images/silhouette.jpg') }
-          style={styles.welcomeImage}
-        />
-        <Text style={styles.welcomeText}>Welcome to your place of rest.</Text>
+        <View>
+          <Image
+            source={ require('../assets/images/silhouette.jpg') }
+            style={styles.welcomeImage}
+          />
+          <Text style={styles.welcomeText}>Welcome to your place of rest.</Text>
+        </View>
+        <View style={styles.twoButtons}>
+          <Button>
+            Login
+          </Button>
+          <Button>
+            Sign up
+          </Button>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create ({
+  twoButtons: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 40
+  },
   welcomeContainer: {
-    marginTop: 50,
+    marginTop: 50
   },
   welcomeText: {
     fontFamily: "Futura",
