22230a3b142ef6535c363205522e8cbab49533fc
tereg <teregalla@gmail.com>
2017-08-16 11:15:56 -0700

    Add react-native-elements and back button to SignUpScreen

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Platform } from 'react-native';
+import { Icon } from 'react-native-elements';
 import Form from '../components/Form';
 
 class SignUpScreen extends Component {
-	static navigationOptions = {
-		title: 'Sign Up'
-	}
+	static navigationOptions = ({ navigation }) => ({
+		title: 'Sign Up',
+		headerLeft:
+			<Icon
+				name='navigate-before'
+				size={32}
+				onPress={ () => navigation.navigate('home')}
+			/>,
+		headerStyle: { marginTop: Platform.OS === 'android' ? 24: 0 }
+	});
 
 	render() {
 		return(
