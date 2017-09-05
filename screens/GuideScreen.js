37602186687631a4e8a8a9fbc8c4023143e2b2d9
tereg <teregalla@gmail.com>
2017-09-05 11:35:58 -0700

    Add grid structure and navigationOptions to GuideScreen

--- screens/GuideScreen.js
+++ screens/GuideScreen.js
@@ -1,19 +1,70 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet, Text, Platform } from 'react-native';
+import { Icon } from 'react-native-elements';
 
 class GuideScreen extends Component {
+	static navigationOptions = ({ navigation }) => ({
+		title: 'Guide',
+		headerLeft:
+			<Icon
+				name='navigate-before'
+				size={32}
+				onPress={ () => navigation.navigate('home') }
+			/>,
+		headerStyle: { marginTop: Platform.OS === 'android' ? 24 : 0 }
+	});
+
+
 	render() {
 		return (
-			<View>
-				<Text>Guide Screen</Text>
-				<Text>Guide Screen</Text>
-				<Text>Guide Screen</Text>
-				<Text>Guide Screen</Text>
-				<Text>Guide Screen</Text>
-				<Text>Guide Screen</Text>
+			<View style={styles.grid}>
+				<View style={styles.row}>
+					<View style={[
+						styles.col,
+						styles.rowOne
+					]}>
+						<Text>Guide Screen</Text>
+					</View>
+					<View style={[
+						styles.col,
+						styles.rowTwo
+					]}>
+						<Text>Guide Screen Two</Text>
+					</View>
+				</View>
+				<View style={styles.row}>
+					<View style={[
+						styles.col,
+						styles.rowThree
+					]}>
+						<Text>Guide Screen Three</Text>
+					</View>
+					<View style={[
+						styles.col,
+						styles.rowFour
+					]}>
+						<Text>Guide Screen Four</Text>
+					</View>
+				</View>
 			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	grid: {
+		flex: 1
+	},
+	row: {
+		flexDirection: 'row'
+	},
+	col: {
+		flexDirection: 'column'
+	},
+	rowOne: { backgroundColor: 'blue'},
+	rowTwo: { backgroundColor: 'red'},
+	rowThree: { backgroundColor: 'green'},
+	rowFour: { backgroundColor: 'yellow'}
+})
 
 export default GuideScreen;
