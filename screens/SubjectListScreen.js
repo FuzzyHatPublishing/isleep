67c57e5829308c2c0857f3c40e23cd19aac3932a
shawn <shawn@naturalprofessional.com>
2017-09-06 13:15:58 -0600

    Add accordion functionality to SubjectListScreen

--- screens/SubjectListScreen.js
+++ screens/SubjectListScreen.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text, Platform } from 'react-native';
+import { View, Text, Platform, StyleSheet } from 'react-native';
 import { Icon } from 'react-native-elements';
-
+import Accordion from 'react-native-collapsible/Accordion';
 
 class SubjectListScreen extends Component {
 	static navigationOptions = ({ navigation }) => ({
@@ -15,24 +15,54 @@ class SubjectListScreen extends Component {
 		headerStyle: { marginTop: Platform.OS === 'android' ? 24 : 0 }
 	});
 
-	state = { subjectData: [] };
-
-	componentWillMount() {
-		let subjectData = this.props.navigation.state.params.subject;
-		this.setState({ subjectData });
+	constructor(props) {
+		super(props);
+		this.state = {
+			title: this.props.navigation.state.params.subject.topic,
+			subjectCollection: this.props.navigation.state.params.subject.subjectCollection
+		}
 	}
+
+	_renderHeader(section) {
+    return (
+      <View style={styles.header}>
+        <Text style={styles.subject}>{section.subject}</Text>
+      </View>
+    );
+  }
+
+  _renderContent(section) {
+    return (
+      <View style={styles.content}>
+        <Text>{section.details}</Text>
+      </View>
+    );
+  }
+
 	render() {
 		return (
 			<View>
-				<Text>Subject List Screen</Text>
-				<Text>Subject List Screen</Text>
-				<Text>Subject List Screen</Text>
-				<Text>Subject List Screen</Text>
-				<Text>Subject List Screen</Text>
-				<Text>Subject List Screen</Text>
-			</View>
+				<Text>{this.state.title}</Text>
+				<Accordion
+	        sections={this.state.subjectCollection}
+	        renderHeader={this._renderHeader}
+	        renderContent={this._renderContent}
+	      />
+	    </View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	header: {
+		backgroundColor: 'tomato'
+	},
+	subject: {
+		backgroundColor: 'yellow'
+	},
+	content: {
+		backgroundColor: 'purple'
+	}
+});
 
 export default SubjectListScreen;
