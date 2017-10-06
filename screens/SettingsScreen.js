e8c10311c7e154fd8a277d7e62faf77afe25d6b6
shawn <shawn@naturalprofessional.com>
2017-10-06 13:07:31 -0600

    Update SettingsScreen. Change more info icon in tab nav.

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,19 +1,140 @@
 import React from 'react';
-import { AsyncStorage } from 'react-native';
-import { ExpoConfigView } from '@expo/samples';
+import { Image, SectionList, StyleSheet, Text, View } from 'react-native';
+import { Constants } from 'expo';
+
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
-    title: 'app.json',
+    title: 'More info',
   };
 
-componentDidMount() {
-	AsyncStorage.removeItem("alreadyLaunched")
-}
+  _renderSectionHeader = ({ section }) => {
+    return <SectionHeader title={section.title} />;
+  };
+
+  _renderItem = ({ item }) => {
+    return (
+      <SectionContent>
+        <Text style={styles.sectionContentText}>
+          {item.value}
+        </Text>
+      </SectionContent>
+    );
+  };
 
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    const { manifest } = Constants;
+    const sections = [
+      { data: [{ value: manifest.version }], title: 'Version'},
+      { data: [{ value: "www.iSleep.rest" }], title: 'Website'},
+      { data: [{ value: "info@fuzzyhatpublishing.com" }], title: 'Contact developers'}
+    ];
+
+    return (
+      <SectionList
+        style={styles.container}
+        renderItem={this._renderItem}
+        renderSectionHeader={this._renderSectionHeader}
+        stickySectionHeadersEnabled={true}
+        keyExtractor={(item, index) => index}
+        ListHeaderComponent={ListHeader}
+        sections={sections}
+      />
+    );
   }
 }
+
+const ListHeader = () => {
+  const { manifest } = Constants;
+
+  return (
+    <View style={styles.titleContainer}>
+      <View style={styles.titleIconContainer}>
+        <AppIconPreview iconUrl={manifest.iconUrl} />
+      </View>
+
+      <View style={styles.titleTextContainer}>
+        <Text style={styles.nameText} numberOfLines={1}>
+          {manifest.name}
+        </Text>
+
+        <Text style={styles.descriptionText}>
+          {manifest.description}
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const SectionHeader = ({ title }) => {
+  return (
+    <View style={styles.sectionHeaderContainer}>
+      <Text style={styles.sectionHeaderText}>
+        {title}
+      </Text>
+    </View>
+  );
+};
+
+const SectionContent = props => {
+  return (
+    <View style={styles.sectionContentContainer}>
+      {props.children}
+    </View>
+  );
+};
+
+const AppIconPreview = ({ iconUrl }) => {
+  return (
+    <Image
+      source={{ uri: iconUrl }}
+      style={{ width: 64, height: 64 }}
+      resizeMode="cover"
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  titleContainer: {
+    paddingHorizontal: 15,
+    paddingTop: 15,
+    paddingBottom: 15,
+    flexDirection: 'row',
+  },
+  titleIconContainer: {
+    marginRight: 15,
+    paddingTop: 2,
+  },
+  sectionHeaderContainer: {
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 8,
+    paddingHorizontal: 15,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#ededed',
+  },
+  sectionHeaderText: {
+    fontSize: 14,
+  },
+  sectionContentContainer: {
+    paddingTop: 8,
+    paddingBottom: 12,
+    paddingHorizontal: 15,
+  },
+  sectionContentText: {
+    color: '#808080',
+    fontSize: 14,
+  },
+  nameText: {
+    fontWeight: '600',
+    fontSize: 18,
+  },
+  descriptionText: {
+    fontSize: 14,
+    marginTop: 6,
+    color: '#4d4d4d',
+  }
+});
