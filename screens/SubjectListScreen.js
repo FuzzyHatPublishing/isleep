164468353d1f754df09379eb64f696a27d41c080
shawn <shawn@naturalprofessional.com>
2017-10-29 11:28:49 -0600

    Make title nav header carry thru from GuideScreen. Style nav header. Add background cloud image to SubjectListScreen.

--- screens/SubjectListScreen.js
+++ screens/SubjectListScreen.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
-import { View, ScrollView, Text, Platform, StyleSheet } from 'react-native';
+import { 
+	Image, 
+	Platform,
+	ScrollView, 
+	StyleSheet,
+	Text,
+	View 
+} from 'react-native';
 import { Icon, List, ListItem } from 'react-native-elements';
 import Accordion from 'react-native-collapsible/Accordion';
 
 class SubjectListScreen extends Component {
-	static navigationOptions = ({ navigation }) => ({
-		title: 'iSleep Guide',
-		headerLeft:
-			<Icon
-				name='navigate-before'
-				size={32}
-				onPress={ () => navigation.goBack() }
-			/>,
-		headerStyle: { marginTop: Platform.OS === 'android' ? 24 : 0 }
-	});
+	static navigationOptions = ({ navigation }) => {
+		const { state: { params = {} } } = navigation;
+		const subjectColor = params.subject.color;
+
+		return { 
+			title: params.subject.topic || 'iSleep Guide', 
+			headerLeft:
+				<Icon
+					name='navigate-before'
+					size={32}
+					color={'white'}
+					onPress={ () => navigation.goBack() }
+				/>,
+			headerStyle: { marginTop: Platform.OS === 'android' ? 24 : 0, backgroundColor: subjectColor },
+			headerTitleStyle: { color: '#fff', fontSize: 24, fontWeight: 'bold' },
+		}
+	};
 
 	constructor(props) {
 		super(props);
@@ -25,7 +39,7 @@ class SubjectListScreen extends Component {
 
 	_renderHeader(section) {
     return (
-    	<View style={styles.header}>
+    	<View style={styles.header}>  
       	<Text style={styles.subject}>{section.subject}</Text>
     	</View>
     );
@@ -33,38 +47,53 @@ class SubjectListScreen extends Component {
 
   _renderContent(section) {
     return (
-      <View style={styles.content}>
+      <View style={styles.content} >
         <Text style={styles.details}>{section.details}</Text>
       </View>
     );
   }
 
 	render() {
+		const backgroundColor = this.props.navigation.state.params.subject.color
 		return (
-			<ScrollView>
-				<Text style={styles.title}>{this.state.title}</Text>
-				<View style={styles.list}>
-					<Accordion
-						underlayColor={'#a3a1a1'}
-		        sections={this.state.subjectCollection}
-		        renderHeader={this._renderHeader}
-		        renderContent={this._renderContent}
-		      />
-		    </View>
-	    </ScrollView>
+			<View style={styles.contentContainerStyle} backgroundColor={backgroundColor}>
+				<Image style={styles.bgImage} source={require('../assets/images/clouds/guide-background-only.png')} />
+					<ScrollView>
+						<View style={styles.list}>
+							<Accordion
+								underlayColor={'#a3a1a1'}
+				        sections={this.state.subjectCollection}
+				        renderHeader={ this._renderHeader}
+				        renderContent={this._renderContent}
+				      />
+				    </View>
+			    </ScrollView>
+		   </View>	
 		);
 	}
 }
 
 const styles = StyleSheet.create({
+	contentContainerStyle: {
+		flex: 1,
+		backgroundColor:"black"
+	},
+	bgImage: {
+		position: 'absolute',
+		top: 0,
+		bottom: 0,
+		left: 0,
+		right: 0
+	},
 	list: {
-		marginTop: 12,
+		// marginTop: 12,
 		borderTopWidth: 1,
 		borderBottomWidth: 1,
 		borderBottomColor: '#cbd2d9'
 	},
 	title: {
 		textAlign: 'center',
+		color: "#fff",
 		fontSize: 22
 	},
 	header: {
@@ -72,21 +101,22 @@ const styles = StyleSheet.create({
 		paddingTop: 10,
 		paddingRight: 10,
 		paddingBottom: 10,
-		borderBottomColor: '#bbb',
+		borderBottomColor: '#555',
 		borderBottomWidth: 1
 	},
 	subject: {
 		fontSize: 16,
-		color: '#43484d',
+		fontWeight: 'bold',
+		color: '#fff',
 		marginLeft: 10,
 		padding: 8
 	},
 	content: {
-		backgroundColor: '#fff',
-		padding: 10
+		padding: 16
 	},
 	details: {
-		fontSize: 14
+		fontSize: 14,
+		color: '#fff'
 	}
 });
 
